d247e237cdfc0ddf6bd8a2545541af8cc5ba16bf
Rakhavendra S M <59560301+Rakhavendra99@users.noreply.github.com>
2020-12-05 00:20:10 +0530

    Add files via upload

---
+++ assignment.php
@@ -0,0 +1,27 @@
+<html>
+<style>
+	*{color:blue;}
+	h2{color:red;}
+	
+</style>
+<body>
+<h2> Assignment operator '='</h2>
+<?php
+	$y =30;
+	$x=$y;
+	echo $x."<br>";
+
+	$x =$x+10;
+	echo $x."<br>";
+
+	$x-=10;
+	echo $x."<br>";
+
+	$x*=2;
+	echo $x."<br>";
+
+
+
+?>
+</body>
+</html>
