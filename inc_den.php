e963e00c5de6af3f17d0d63b0598627ef9c590e5
Rakhavendra S M <59560301+Rakhavendra99@users.noreply.github.com>
2020-12-05 00:34:23 +0530

    Add files via upload

---
+++ inc_den.php
@@ -0,0 +1,29 @@
+<html>
+<style>
+	*{color:blue;}
+	h2{color:red;}
+	
+</style>
+<body>
+<h2> Increment / Decrement operator</h2>
+<?php
+	$x =10;
+	echo $x."<br>";
+	
+	echo $x++."<br>"; //post increment
+	echo $x."<br>";
+	
+	echo ++$x."<br>"; // pre increment
+	echo $x."<br>";
+	
+	echo $x--."<br>"; //post decrement
+	echo $x."<br>";
+	
+	echo --$x."<br>"; //pre decrement 
+	echo $x."<br>";
+
+
+
+?>
+</body>
+</html>
