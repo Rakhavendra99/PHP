ce63e40523a2a8e778b373b5e19eb00086002cee
Rakhavendra S M <59560301+Rakhavendra99@users.noreply.github.com>
2020-12-04 23:57:14 +0530

    Add files via upload

---
+++ arthimatic.php
@@ -0,0 +1,15 @@
+<?php
+ # + - * / % **
+ 
+ $x = 20;
+ $y = 10;
+ echo $x + $y."<br>";
+ echo $x - $y."<br>";
+ echo $x * $y."<br>";
+ echo $x / $y."<br>";
+echo $x % $y."<br>";
+echo $x ** 2;
+
+
+?>
+
